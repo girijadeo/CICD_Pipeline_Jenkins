cb0223179bb66278f402b537be5418bdc5e676cf
girijadeo <32549451+girijadeo@users.noreply.github.com>
2017-10-05 13:23:09 -0500

    Added assert

--- src/test/java/pageObjects/NewAppHomePage.java
+++ src/test/java/pageObjects/NewAppHomePage.java
@@ -1,8 +1,12 @@
 package pageObjects;
 
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.log4testng.Logger;
 
 import com.cognizant.Craft.ReusableLibrary;
 import com.cognizant.Craft.ScriptHelper;
@@ -36,12 +40,16 @@ public class NewAppHomePage extends ReusableLibrary{
 	}
 	
 	public void verifyHomePageHeaderDisplayed() {
-		if(Utility_Functions.xWaitForElementClickable(driver, HEADER, 10)) {
-			System.out.println("Login successful, Home page displayed");
-		}
-		else {
-			System.out.println("Home page not displayed");
-		}
+//		if(Utility_Functions.xWaitForElementClickable(driver, HEADER, 10)) {
+//			System.out.println("Login successful, Home page displayed");			
+			Assert.assertTrue(Utility_Functions.xWaitForElementClickable(driver, HEADER, 10),  "Home page not displayed on logging in");
+			System.out.println("Home page displayed on logging in");
+			
+
+//		}
+//		else {
+//			System.out.println("Home page not displayed");
+//		}
 	}
 	
 }
